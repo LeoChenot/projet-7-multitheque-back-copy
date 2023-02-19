e4d4bb11db7eef4d45dcd7de2ee4fd51efdcb7bc
LeoChenot <leochenot1@gmail.com>
2023-02-19 19:48:40 +0100

    change psql url

--- app/model/dataClient.js
+++ app/model/dataClient.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const client = new Pool({
-  connectionString: 'postgres://hbjxklcdgtyfom:39a01cfee918fa7729f83dc6155c4dcb6cd6bb304f57fa405cdaaec69d4c986e@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d2s4rd97gne0v9',
+  connectionString: 'postgres://collectio:OpV0dLz9RuBt@ep-lingering-forest-229253.eu-central-1.aws.neon.tech/collectio',
   ssl:{
     rejectUnauthorized:false
   }
